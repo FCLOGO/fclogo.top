207944e4baeadd31f900a8f4610988537abacee2
Ryan <iii.ryan@outlook.com>
2021-10-04 17:05:28 +0800

    feat: :sparkles: adding 404 page
    adding 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import * as React from 'react'
+import { useIntl } from 'react-intl'
+import { LocalizedLink } from 'gatsby-plugin-usei18n'
 
-export default function Notfound() {
+import Layout from '../components/layout'
+import Seo from '../components/seo'
+import Search from '../components/single-search'
+
+import ArrowIcon from '../../static/assets/icons/arrowForward.inline.svg'
+
+import { mainContent, title, subtitle, description, backLink } from './404.module.styl'
+
+const NotFoundPage = ({ pageContext }) => {
+  const intl = useIntl()
   return (
-    <div>
-      <p>Noting</p>
-    </div>
+    <Layout pageContext={pageContext}>
+      <Seo title={intl.formatMessage({ id: 'home.notFound' })} />
+      <Search locale={pageContext.locale} />
+      <div className={mainContent}>
+        <h1 className={title}>404</h1>
+        <h2 className={subtitle}>{intl.formatMessage({ id: 'home.ooops' })}</h2>
+        <p className={description}>{intl.formatMessage({ id: 'home.notFoundText' })}</p>
+        <LocalizedLink to="/" className={backLink}>
+          <ArrowIcon />
+          <span>{intl.formatMessage({ id: 'home.backHome' })}</span>
+        </LocalizedLink>
+      </div>
+    </Layout>
   )
 }
+
+export default NotFoundPage
