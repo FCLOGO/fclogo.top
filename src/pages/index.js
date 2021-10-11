b44a1ea6f7a4196ef945d8f98e4f2d7ed7cee282
Ryan <iii.ryan@outlook.com>
2021-10-11 17:30:42 +0800

    revert: :sparkles: restore the previous version

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,38 +10,12 @@ import RandomList from '../components/index-random'
 
 import { mainContent } from './index.module.styl'
 
-import incrementViews from '../lib/increment-downloads'
-import app from 'gatsby-plugin-firebase-v9.0'
-import { getDatabase, ref, set } from 'firebase/database'
-import {
-  initializeAppCheck,
-  ReCaptchaV3Provider,
-  getToken,
-  AppCheckToken,
-  AppCheckTokenResult
-} from 'firebase/app-check'
-
 const IndexPage = ({ data, pageContext }) => {
   const intl = useIntl()
-  const appCheck = initializeAppCheck(app, {
-    provider: new ReCaptchaV3Provider(process.env.GATSBY_RECAPTCHA_SITE_KEY),
-    isTokenAutoRefreshEnabled: true
-  })
-  getToken(appCheck)
-    .then(() => {
-      console.log('success')
-    })
-    .catch(error => {
-      console.log(error.code)
-    })
-
-  const db = getDatabase(app)
-  const Push = () => incrementViews('test')
   return (
     <Layout pageContext={pageContext}>
       <Seo title={intl.formatMessage({ id: 'home.title' })} />
       <Hero totalCount={data.allLogo.totalCount} locale={pageContext.locale} />
-      <button onClick={Push}>PUSH</button>
       <div className={mainContent}>
         <LogoList data={data} />
         {data.allLogo.nodes.length > 50 ? <RandomList data={data} /> : ''}
