6a01bdffdd977ff8fb51ccb75e72c129b7213e65
Ryan <iii.ryan@outlook.com>
2023-10-17 15:20:15 +0800

    style: :art: 修改 Twitter 图标为 X

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import { Link, useTranslation } from 'gatsby-plugin-react-i18next'
 
 import LogoIcon from '../../static/assets/icons/fclogo.inline.svg'
 import GithubIcon from '../../static/assets/icons/github.inline.svg'
-import TwitterIcon from '../../static/assets/icons/twitter.inline.svg'
+import XIcon from '../../static/assets/icons/x.inline.svg'
 import WeiboIcon from '../../static/assets/icons/weibo.inline.svg'
 import DiscordIcon from '../../static/assets/icons/discord.inline.svg'
 
@@ -14,9 +14,9 @@ const SocialMedia = [
     icon: <GithubIcon className="h-2xl w-2xl" />
   },
   {
-    title: 'Twitter',
+    title: 'X',
     link: 'https://twitter.com/fclogotop',
-    icon: <TwitterIcon className="h-2xl w-2xl" />
+    icon: <XIcon className="h-2xl w-2xl" />
   },
   {
     title: 'Weibo',
