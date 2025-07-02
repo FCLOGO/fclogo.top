6be597cca7d4765856314f3485e11ca87992b5af
iiiRyan <85ryan.blue@gmail.com>
2025-07-02 10:59:41 +0800

    chore: 添加**新闻博客**及**贡献徽标**链接

--- src/components/_header/main-menu.js
+++ src/components/_header/main-menu.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link, useTranslation } from 'gatsby-plugin-react-i18next'
 
 import LanguageSwitcher from '../_header/language-switcher'
+import MergeIcon from '../../../static/assets/icons/merge.inline.svg'
 
 const MenuLink = props => (
   <li className="tablet:w-full">
@@ -22,11 +23,30 @@ const MainMenu = () => {
         <MenuLink to="/logos">{t('menu.logo')}</MenuLink>
         <MenuLink to="/packs">{t('menu.pack')}</MenuLink>
         <MenuLink to="/logomap">{t('menu.map')}</MenuLink>
+        <li className="tablet:w-full">
+          <a
+            className="relative uppercase inline-block ml-lg text-gray hover:text-white font-medium tracking-widest tablet:w-full tablet:h-[48px] tablet:leading-[48px] tablet:text-dark-gray tablet:hover:text-dark-gray"
+            href="https://news.fclogo.top/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {t('menu.news')}
+          </a>
+        </li>
         <MenuLink to="/contribution">{t('menu.contribution')}</MenuLink>
         <MenuLink to="/links">{t('menu.links')}</MenuLink>
         <MenuLink to="/about">{t('menu.about')}</MenuLink>
       </ul>
-      <LanguageSwitcher />
+      <div className="flex flex-nowrap justify-center items-center">
+        <LanguageSwitcher />
+        <Link
+          to="/support/how-to-contribute-a-logo"
+          className="flex-initial px-md rounded inline-flex items-center justify-center text-white bg-green hover:bg-light-green ml-md h-3xl leading-3xl tablet:hidden"
+        >
+          <MergeIcon className="stroke-white h-lg w-lg mr-sm" />
+          {t('menu.contribute')}
+        </Link>
+      </div>
     </div>
   )
 }
